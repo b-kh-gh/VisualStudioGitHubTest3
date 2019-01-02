bd07afd4c7ebbe734f40842dce7e6730c6795bab
ali <ali@ALI-PC>
2019-01-02 11:37:48 +0330

    Revert "Revert "FIrst Commit""
    This reverts commit b34cf4fd5f2f4a0d0bed00011f3d518f5a7331e2.

--- VisualStudioGitTest3/Controllers/HomeController.cs
+++ VisualStudioGitTest3/Controllers/HomeController.cs
@@ -6,10 +6,12 @@ using System.Web.Mvc;
 
 namespace VisualStudioGitTest3.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : Controller   
     {
         public ActionResult Index()
         {
+
+
             return View();
         }
 
